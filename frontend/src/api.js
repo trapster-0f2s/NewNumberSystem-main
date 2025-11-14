d84fff95b40c7dabe809cf7dbdb6f15f52edaaea
Rob <madalakalomho123@gmail.com>
2025-11-14 15:27:00 +0200

    Added backend api link

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,1 +1,18 @@
- 
+const API_URL = process.env.REACT_APP_API_URL || "https://numbersystembackend.onrender.com";
+
+export async function loginUser(credentials) {
+  return fetch(`${API_URL}/api/auth/login`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(credentials),
+  }).then((data) => data.json());
+}
+
+export async function registerUser(credentials) {
+  return fetch(`${API_URL}/api/auth/signup`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(credentials),
+  }).then((data) => data.json());
+}
+
